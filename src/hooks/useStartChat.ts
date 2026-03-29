7ceb1fbb1a5eb9e177dae515a8e403337ba854a5
Duclaux Tagne <tagneduclaux@gmail.com>
2026-03-29 16:44:54 +0100

    Implementing explore screen

---
+++ src/hooks/useStartChat.ts
@@ -0,0 +1,35 @@
+import { useRouter } from "expo-router";
+import { Alert } from "react-native";
+import { Channel, StreamChat } from "stream-chat";
+
+type UseStartChatParams = {
+    client: StreamChat,
+    userId: string,
+    setChannel: (channel: Channel) => void,
+    setCreating: (value: string | null) => void;
+}
+
+const useStartChat = ({client, userId, setChannel, setCreating}: UseStartChatParams) => {
+    const router = useRouter();
+    const handleStartChat = async (targetId: string) => {
+        setCreating(targetId);
+
+        try {
+            const channel = client.channel("messaging", {members: [userId, targetId]});
+            await channel.watch();
+
+            setChannel(channel);
+            // todo: make this work later
+            // router.push(`/channel/${channel.cid}`);
+        } catch (error) {
+            console.log("Error creating chat:", error);
+            Alert.alert("Error", "Couldn't create chat. Please try again.")            
+        } finally {
+            setCreating(null);
+        }
+    };
+
+  return { handleStartChat };
+}
+
+export default useStartChat
